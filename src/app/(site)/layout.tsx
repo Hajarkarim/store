f61350cd84bd0806ed0aa44bae6019ed42fc9834
hajar <hajourathajar00@gmail.com>
2025-09-30 18:18:18 +0100

    removing test files

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
                     {children}
 
                     <QuickViewModal />
-                    <CartSidebarModal />
                     <PreviewSliderModal />
                   </PreviewSliderProvider>
                 </ModalProvider>
