5e101e7f714cdbc8b980932337259fb143b0f34a
hajar <hajourathajar00@gmail.com>
2025-09-30 18:34:01 +0100

    removing test files

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,15 +4,11 @@ import "../css/euclid-circular-a-font.css";
 import "../css/style.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-
 import { ModalProvider } from "../context/QuickViewModalContext";
-import { CartModalProvider } from "../context/CartSidebarModalContext";
 import { ReduxProvider } from "@/redux/provider";
 import QuickViewModal from "@/components/Common/QuickViewModal";
-import CartSidebarModal from "@/components/Common/CartSidebarModal";
 import { PreviewSliderProvider } from "../context/PreviewSliderContext";
 import PreviewSliderModal from "@/components/Common/PreviewSlider";
-
 import ScrollToTop from "@/components/Common/ScrollToTop";
 import PreLoader from "@/components/Common/PreLoader";
 //layout
@@ -35,7 +31,6 @@ export default function RootLayout({
         ) : (
           <>
             <ReduxProvider>
-              <CartModalProvider>
                 <ModalProvider>
                   <PreviewSliderProvider>
                     <Header />
@@ -45,7 +40,6 @@ export default function RootLayout({
                     <PreviewSliderModal />
                   </PreviewSliderProvider>
                 </ModalProvider>
-              </CartModalProvider>
             </ReduxProvider>
             <ScrollToTop />
             <Footer />
